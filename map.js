fa87213b5eea2d34436313654b6fca2425c3165d
sk-miller <71351189+sk-miller@users.noreply.github.com>
2024-03-13 19:04:49 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,30 @@
+// Initialize the map
 let map = L.map('map').setView([51.0447, -114.0719], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+
+// Add a base map layer
+let baseLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11', // or any other style you prefer
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2stbWlsbGVyIiwiYSI6ImNsdG0zb215NzFrYTEybG80bWcybmRzcjUifQ.q4-a3Zg4fNWMx0nM5tmUzQ'
 }).addTo(map);
+
+// Add your custom layer
+let tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    id: 'sk-miller/cltm420bt00sj01r5gn5t498c',
+    accessToken: 'pk.eyJ1Ijoic2stbWlsbGVyIiwiYSI6ImNsdG0zb215NzFrYTEybG80bWcybmRzcjUifQ.q4-a3Zg4fNWMx0nM5tmUzQ'
+});
+
+// Function to toggle the custom layer on and off
+document.getElementById('toggleLayer').addEventListener('change', function() {
+    if (this.checked) {
+        tileLayer.addTo(map);
+    } else {
+        map.removeLayer(tileLayer);
+    }
+});
 
 let markers = L.markerClusterGroup();
 map.addLayer(markers);
